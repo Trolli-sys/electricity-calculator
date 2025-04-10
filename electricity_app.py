7a5e9858f132a493af2e0cece82215f304c14167
Trolli-sys <anawat.514831@gmail.com>
2025-04-10 16:17:04 +0700

    electricity_app.py

--- electricity_app.py
+++ electricity_app.py
@@ -818,7 +818,7 @@ if st.session_state.full_dataframe is not None and not st.session_state.full_dat
 if st.session_state.calculation_result:
     st.markdown("---")
     st.header("3. ผลการคำนวณ")
-    st.code("รายละเอียดค่าไฟฟ้าโดยประมาณ:", st.session_state.calculation_result, language=None, height=450, key="result_text_area", disabled=True)
+    st.code("รายละเอียดค่าไฟฟ้าโดยประมาณ:", st.session_state.calculation_result, language=None, height=450, key="result_text_area")
 
     # เพิ่มปุ่มสำหรับดาวน์โหลดผลลัพธ์เป็นไฟล์ txt
     try:
